11087e7efbc75b6b4ce488e0377ac098b37d9002
42decibel <39693451+JoHyukJun@users.noreply.github.com>
2024-10-07 20:44:50 +0900

    feat: Init prj

---
+++ Musiqueue/Musiqueue/MusiqueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusiqueueApp.swift
+//  Musiqueue
+//
+//  Created by johyukjun on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MusiqueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
