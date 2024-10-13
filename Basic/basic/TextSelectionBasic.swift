aca333eeed3bc557ccb437116a3ff056a918fd86
42decibel <39693451+JoHyukJun@users.noreply.github.com>
2024-10-13 20:11:09 +0900

    feat: Add basic rsc

---
+++ Basic/basic/TextSelectionBasic.swift
@@ -0,0 +1,20 @@
+//
+//  TextSelectionBasic.swift
+//  Basic
+//
+//  Created by johyukjun on 10/13/24.
+//
+
+import SwiftUI
+
+struct TextSelectionBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.title2)
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelectionBasic()
+}
