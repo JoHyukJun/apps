591d86d81eff4c93de1f0ea071aece3602f694fe
42decibel <39693451+JoHyukJun@users.noreply.github.com>
2024-10-17 20:32:55 +0900

    feat: Add basic rsc

---
+++ Basic/basic/MultiDatePickerBasic.swift
@@ -0,0 +1,18 @@
+//
+//  MultiDatePickerBasic.swift
+//  Basic
+//
+//  Created by johyukjun on 10/15/24.
+//
+
+import SwiftUI
+
+struct MultiDatePickerBasic: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MultiDatePickerBasic()
+}
