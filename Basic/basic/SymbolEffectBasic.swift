c233704ce95bd8f79856ca16ae57581d016d4494
42decibel <39693451+JoHyukJun@users.noreply.github.com>
2024-10-30 17:45:20 +0900

    feat: Add basic rsc

--- Basic/basic/SymbolEffectBasic.swift
+++ Basic/basic/SymbolEffectBasic.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct SymbolEffectBasic: View {
+    //property
+    @State private var startAnimation: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            Label(
+                title: { Text("variableColor") },
+                icon: {
+                    Image(systemName: "airplayaudio")
+                        .foregroundStyle(.mint)
+                        .symbolEffect(.variableColor)
+                }
+            )
+            
+            Label(
+                title: { Text("Pulse") },
+                icon: {
+                    Image(systemName: "airplayaudio.badge.exclamationmark")
+                        .foregroundStyle(.red, .mint)
+                        .symbolEffect(.pulse)
+                }
+            )
+            
+            Label(
+                title: { Text("button") },
+                icon: {
+                    Image(systemName: "lightbulb.max.fill")
+                        .foregroundStyle(.mint)
+                        .symbolEffect(.bounce, options: .repeating.repeat(2), value: startAnimation)
+                }
+            )
+            
+            Button("Start Animation") {
+                startAnimation.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .font(.title)
+        .listStyle(.plain)
     }
 }
 
