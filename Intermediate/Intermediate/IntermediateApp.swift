2e30309d68a8e4012e272fecbf3fe220a0a159b0
42decibel <39693451+JoHyukJun@users.noreply.github.com>
2024-10-31 10:42:00 +0900

    feat: Init project

---
+++ Intermediate/Intermediate/IntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermediateApp.swift
+//  Intermediate
+//
+//  Created by johyukjun on 10/30/24.
+//
+
+import SwiftUI
+
+@main
+struct IntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
