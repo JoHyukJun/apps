8a66d8d7754d9022a45bab2a304300e211c27a11
42decibel <39693451+JoHyukJun@users.noreply.github.com>
2024-11-03 13:24:19 +0900

    feat: Add inter rsc

---
+++ Intermediate/Intermediate/SwiftUI Core/DragGestureInter.swift
@@ -0,0 +1,18 @@
+//
+//  DragGestureInter.swift
+//  Intermediate
+//
+//  Created by johyukjun on 11/2/24.
+//
+
+import SwiftUI
+
+struct DragGestureInter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DragGestureInter()
+}
---
+++ Intermediate/Intermediate/SwiftUI Core/MatchedGeometryEffectInter.swift
@@ -0,0 +1,18 @@
+//
+//  MatchedGeometryEffectInter.swift
+//  Intermediate
+//
+//  Created by johyukjun on 11/2/24.
+//
+
+import SwiftUI
+
+struct MatchedGeometryEffectInter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MatchedGeometryEffectInter()
+}
