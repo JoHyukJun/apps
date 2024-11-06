38df09c7ce849a5e79fa4793897e8f57140fa5e0
42decibel <39693451+JoHyukJun@users.noreply.github.com>
2024-11-06 21:48:24 +0900

    feat: Add inter rsc

--- Intermediate/Intermediate/SwiftUI Core/ViewModifierInter.swift
+++ Intermediate/Intermediate/SwiftUI Core/ViewModifierInter.swift
@@ -7,9 +7,51 @@
 
 import SwiftUI
 
+
+struct DefaultButtonModifier: ViewModifier {
+    let backgroundColor: Color
+    
+    // ViewModifier 에 body 가 들어가는데 some View 를 return 하는 함수형태의 body
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .frame(height: 60)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(backgroundColor)
+    }
+}
+
 struct ViewModifierInter: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            // general button
+            Button {
+                
+            } label: {
+                Text("ViewModifier Practice 1")
+                    .withDefaultButtonFormat()
+            }
+            
+            Button {
+                
+            } label: {
+                Text("ViewModifier Practice 2")
+                    .withDefaultButtonFormat(backgroundColor: .yellow)
+            }
+            
+            Button {
+                
+            } label: {
+                Text("ViewModifier Practice 3")
+                    .withDefaultButtonFormat()            }
+        }
+    }
+}
+
+extension View {
+    func withDefaultButtonFormat(backgroundColor: Color = Color.green) -> some View {
+        self.modifier(DefaultButtonModifier(backgroundColor: backgroundColor))
     }
 }
 
