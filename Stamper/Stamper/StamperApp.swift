a87bcfb211326b50f9b67ddb1624b8fe43ef6f72
42decibel <39693451+JoHyukJun@users.noreply.github.com>
2024-11-08 22:08:17 +0900

    feat: Init project

---
+++ Stamper/Stamper/StamperApp.swift
@@ -0,0 +1,17 @@
+//
+//  StamperApp.swift
+//  Stamper
+//
+//  Created by johyukjun on 11/8/24.
+//
+
+import SwiftUI
+
+@main
+struct StamperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
