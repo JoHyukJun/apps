fdd8a58817aa01a9f56883b686ef4ad4e5348201
42decibel <39693451+JoHyukJun@users.noreply.github.com>
2024-11-11 18:03:07 +0900

    feat: Init project

---
+++ Menuote/Menuote/App/MenuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuoteApp.swift
+//  Menuote
+//
+//  Created by johyukjun on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MenuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
