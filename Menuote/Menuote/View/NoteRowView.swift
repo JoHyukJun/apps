088c026f5c04a36de76b1be5e57771ef0c64384b
42decibel <39693451+JoHyukJun@users.noreply.github.com>
2024-11-14 21:58:39 +0900

    feat: Add menuote rsc

---
+++ Menuote/Menuote/View/NoteRowView.swift
@@ -0,0 +1,18 @@
+//
+//  NoteRowView.swift
+//  Menuote
+//
+//  Created by johyukjun on 11/14/24.
+//
+
+import SwiftUI
+
+struct NoteRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteRowView()
+}
